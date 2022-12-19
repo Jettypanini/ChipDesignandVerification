7c060da1b37ace6a510ced33142dad6ca5c3fde1
Jettypanini <jethro.pans@hotmail.com>
2022-12-19 09:47:34 +0100

    if

--- ALU_project/gameboyprocessor.sv
+++ ALU_project/gameboyprocessor.sv
@@ -59,93 +59,95 @@ class gameboyprocessor;
 
         forever
         begin
-
-            this.gen2mdl.get(tra);
-            s = $sformatf("[%t | MDL] received and calculating: %s", $time, tra.toString());
-            $display(s);
-            this.instruction_type = tra.instruction[7:6];
-            this.instruction_selection = tra.instruction[5:3];
-            this.operand_selection = tra.instruction[2:0];
-            s = $sformatf("[%t | MDL] instruction type: %x", $time, this.instruction_type);
-            $display(s);
-            s = $sformatf("[%t | MDL] instruction selection: %x", $time, this.instruction_selection);
-            $display(s);
-            s = $sformatf("[%t | MDL] operand selection: %x", $time, this.operand_selection);
-            $display(s);
-
-            if (this.instruction_type == 2)
-            begin
-              case (operand_selection)
-                0 : begin
-                      val_reg = this.B;
-                    end
-                1 : begin
-                      val_reg = this.C;
-                    end
-                2 : begin
-                      val_reg = this.D;
-                    end
-                3 : begin
-                      val_reg = this.E;
-                    end
-                4 : begin
-                      val_reg = this.H;
-                    end
-                5 : begin
-                      val_reg = this.L;
-                    end
-                // Value of HL is ignored.
-                //6 : begin
-                //      val_reg = HL;
-                //    end
-                7 : begin
-                      val_reg = this.A;
-                    end
-              endcase
-              
-              case(instruction_selection)
-                0:  begin
-                      this.A = this.A + val_reg;
-                    end
-                1:  begin
-                      this.A = this.A + val_reg;
-                    end
-                2:  begin
-                      this.A = this.A - val_reg;
-                    end
-                3:  begin
-                      this.A = this.A - val_reg;
-                    end
-                4:  begin
-                      this.A = this.A & val_reg;
-                    end
-                5:  begin
-                      this.A = this.A ^ val_reg;
-                    end
-                6:  begin
-                      this.A = this.A | val_reg;
-                    end
-                7:  begin
-                      this.A = this.A + val_reg;
-                    end
-              endcase
-            end
-
-            probe.regA = this.A;
-            probe.regB = this.B;
-            probe.regC = this.C;
-            probe.regD = this.D;
-            probe.regE = this.E;
-            probe.regF = this.F;
-            probe.regH = this.H;
-            probe.regL = this.L;
-
-            s = $sformatf("[%t | MDL] I calculated with %x", $time, tra.instruction);
-            $display(s);
-            probe.show();
-
-            @(this.ifc.valid == 1)
-            this.mdl2chk.put(probe);
+          if (this.mdl2chk.num() == 0)
+          begin
+					  this.gen2mdl.get(tra);
+					  s = $sformatf("[%t | MDL] received and calculating: %s", $time, tra.toString());
+					  $display(s);
+					  this.instruction_type = tra.instruction[7:6];
+					  this.instruction_selection = tra.instruction[5:3];
+					  this.operand_selection = tra.instruction[2:0];
+					  s = $sformatf("[%t | MDL] instruction type: %x", $time, this.instruction_type);
+					  $display(s);
+					  s = $sformatf("[%t | MDL] instruction selection: %x", $time, this.instruction_selection);
+					  $display(s);
+					  s = $sformatf("[%t | MDL] operand selection: %x", $time, this.operand_selection);
+					  $display(s);
+		
+					  if (this.instruction_type == 2)
+					  begin
+					  case (operand_selection)
+						  0 : begin
+							  val_reg = this.B;
+				  			  end
+				  		1 : begin
+				  			val_reg = this.C;
+				  			end
+				  		2 : begin
+				  			val_reg = this.D;
+				  			end
+				  		3 : begin
+			  				val_reg = this.E;
+			  				end
+			  			4 : begin
+			  				val_reg = this.H;
+			  				end
+			  			5 : begin
+			  				val_reg = this.L;
+			  				end
+			  			// Value of HL is ignored.
+			  			//6 : begin
+			  			//      val_reg = HL;
+			  			//    end
+				  		7 : begin
+				  			val_reg = this.A;
+				  			end
+				  	endcase
+					
+			  		case(instruction_selection)
+			  			0:  begin
+			  				this.A = this.A + val_reg;
+			  				end
+			  			1:  begin
+			  				this.A = this.A + val_reg;
+			  				end
+			  			2:  begin
+			  				this.A = this.A - val_reg;
+			  				end
+		  				3:  begin
+				  			this.A = this.A - val_reg;
+							end
+			  			4:  begin
+			  				this.A = this.A & val_reg;
+							end
+			  			5:  begin
+		  					this.A = this.A ^ val_reg;
+							end
+			  			6:  begin
+			  				this.A = this.A | val_reg;
+							end
+		  				7:  begin
+		  					this.A = this.A + val_reg;
+							end
+		  			endcase
+					  end
+		
+					  probe.regA = this.A;
+					  probe.regB = this.B;
+					  probe.regC = this.C;
+					  probe.regD = this.D;
+					  probe.regE = this.E;
+					  probe.regF = this.F;
+					  probe.regH = this.H;
+					  probe.regL = this.L;
+		
+					  s = $sformatf("[%t | MDL] I calculated with %x", $time, tra.instruction);
+					  $display(s);
+					  probe.show();
+		
+					  @(this.ifc.valid == 1)
+					  this.mdl2chk.put(probe);
+          end
         end
 
     endtask : executeALUInstruction
